ee99e83e3d7b48cafc775868ddbf2dd0f6b9081f
udaibirv <udaibirv@gmail.com>
2021-06-09 18:22:07 -0700

    targeted the right data for league standings but work in progress for rendering them onto page.

--- client/pages/england.jsx
+++ client/pages/england.jsx
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 export default class England extends React.Component{
   constructor(props){
     super(props);
+    this.state = {
+      england: []
+    }
+    this.getEnglandInfo = this.getEnglandInfo.bind(this);
+
+  }
+
+
+  componentDidMount(){
+  this.getEnglandInfo();
+
+  }
+
+
+
+  getEnglandInfo() {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch('/api/leauge-info/england', requestOptions)
+      .then(response => response.json())
+      .then(result => {
+
+        const table = result.response[0].league.standings;
+        this.setState({england: table});
+
+        // console.log(table);
+      })
+      .catch(error => console.log('error', error));
+
   }
 
 render(){
+  const { england } = this.state;
   return (
-    <h1>Hello </h1>
-  )
+    england.map((team, rank) => {
+      return (
+        <div key={rank} className="col-12 col-md-6 col-lg-4">
+          <table>
+            <tr key={team.rank}>
+              <td>{england}</td>
+            </tr>
+          </table>
+        </div>
+      );
+
+    })
+
+  );
 }
 }

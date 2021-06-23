e516b1d7f972cfecca9d7b54aacb66c6b6634b83
udaibirv <udaibirv@gmail.com>
2021-06-22 18:05:49 -0700

    need to finish building out league table.

--- client/pages/england.jsx
+++ client/pages/england.jsx
@@ -1,6 +1,5 @@
+import { info } from 'loglevel';
 import React, { useState, useEffect } from 'react';
-import ReactTable from 'react-table';
-
 export default class England extends React.Component {
   constructor(props) {
     super(props);
@@ -27,48 +26,86 @@ export default class England extends React.Component {
       .then(result => {
 
         const table = result.response[0].league.standings;
-        table.map((club, index) => {
-          return (
-            this.setState({ england: club[index] })
-          );
-        });
+        this.setState({ england: table });
 
-        console.log(this.state);
+        console.log(this.state.england);
       })
       .catch(error => console.log('error', error));
 
   }
 
   render() {
+    const { england } = this.state;
     return (
-      <h1>hello</h1>
-    );
-    // const { england } = this.state;
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Position</th>
+            <th scope="col">Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
 
-    // const data = england.map((info, j) => {
+              {england.map((info, j) => {
+                return (
+                  info.map((club, x) => {
+                    return (
+                      <th key={x}>
+                        <td>
+                          {club.rank}
+                        </td>
+                      </th>
+                    );
+                  })
+                );
+              })
+            }
+
+          </tr>
+        </tbody>
+      </table>
+
+    // england.map((club, index) => {
     //   return (
-    //     <div key={j}>
-    //       <h1>info.team.name</h1>
+    //   <div key={index}>
+    //     {club.map((info, j) => {
+    //       return (
+    //     <div key={info[j]}>
+    //         <table className="table">
+    //           <thead>
+
+    //               <th scope="row">Position</th>
+    //               {/* <th scope="col">Team</th>
+    //               <th scope="col">Played</th>
+    //               <th scope="col">Win</th>
+    //               <th scope="col">Loss</th>
+    //               <th scope="col">Draw</th>
+    //               <th scope="col">GF</th>
+    //               <th scope="col">GA</th>
+    //               <th scope="col">Points</th> */}
+
+    //           </thead>
+    //           <tbody>
+
+    //               <td scope="row">{info.rank}</td>
+    //               {/* <td scope="row">{info.team.name}</td>
+    //               <td scope="row">{info.all.played}</td>
+    //               <td scope="row">{info.all.win}</td>
+    //               <td scope="row">{info.all.lose}</td>
+    //               <td scope="row">{info.all.draw}</td>
+    //               <td scope="row">{info.all.goals.for}</td>
+    //               <td scope="row">{info.all.goals.against}</td>
+    //               <td scope="row">{info.points}</td> */}
+
+    //           </tbody>
+    //         </table>
     //     </div>
+    //       );
+    //     })}
+    //   </div>
     //   );
-    // });
-    // return (
-    //   <h1>Hello</h1>
-    //   // england.map((club, index) => {
-    //   //   return (
-    //   //     <div key={index}>
-    //   //       <h1>{club[index].team.name}</h1>
-    //   //       {club.map((info, j) => {
-    //   //         return (
-    //   //       <div key={j}>
-    //   //         <h2 key={j}>{info.team.name}</h2>
-    //   //           <p>{info.rank}</p>
-    //   //       </div>
-    //   //         );
-    //   //       })}
-    //   //     </div>
-    //   //   );
-    //   // })
-    // );
+    // })
+    );
   }
 }
